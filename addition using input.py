f7a0f6ebb974e73e6bff6b8205de4f02a5c9c076
Ghanshyamsinh Zala <130747789+GZ30eee@users.noreply.github.com>
2024-01-14 22:52:32 +0530

    Add files via upload

---
+++ addition using input.py
@@ -0,0 +1,6 @@
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+
+sum = num1 + num2
+12
+print("THE SUM IS: ", sum)
---
+++ addition without input function.py
@@ -0,0 +1,6 @@
+num1 = 10
+num2 = 9
+
+sum = num1 + num2
+
+print("THE SUM IS: ",sum)
