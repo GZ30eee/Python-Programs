7c8d130b9b4c8ec7f4c5515d916acc5e42328927
Ghanshyamsinh Zala <130747789+GZ30eee@users.noreply.github.com>
2024-02-19 23:29:42 +0530

    Python

---
+++ escape.py
@@ -0,0 +1,26 @@
+# Single Quote
+print('It\'s a beautiful day!')
+
+# Backslash
+print('This is a backslash: \\\.')
+
+# New Line
+print('Hello\nWorld!')
+
+# Carriage Return
+print('Hello\rWorld!')
+
+# Tab
+print('Hello\tWorld!')
+
+# Backspace
+print('Hello \bWorld!')
+
+# Form Feed
+print('Hello \fWorld!')
+
+# Octal value
+print('\141\142\143')
+
+# Hex value
+print('\x41\x42\x43')
